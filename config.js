a0336c010ad3ddc111a8f7d5f5580aca2f8417a0
nysoabtp <wayzamagcom@gmail.com>
2026-06-09 12:39:05 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // Copier config.example.js, renommer en config.js et remplir.
 // ============================================================
 const SUPABASE_URL = 'https://djncsybvloyyesllfxhq.supabase.co';
-const SUPABASE_KEY = 'REMPLACER_PAR_VOTRE_CLE_ANON_SUPABASE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqbmNzeWJ2bG95eWVzbGxmeGhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NDI1OTksImV4cCI6MjA5MzMxODU5OX0.o4MOSg6axYoNdl0XgidLi0eNukR-KwnKvecZxchkcP8';
